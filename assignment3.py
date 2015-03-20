e9468b1c6512a3c597c610573c0b9ef0d21d25b6
GeStanley <geoffrey.stanley@gmail.com>
2015-03-20 17:22:11 -0400

    working

--- assignment3.py
+++ assignment3.py
@@ -15,17 +15,19 @@ if __name__ == '__main__':
     dimensions = pylab.arange(249, 1, -1)
     reconstruction_error = numpy.zeros((248,),dtype=numpy.float)
 
-    for components in dimensions:
+    for d in dimensions:
 
-        pca = PCA(components)
+        pca = PCA(d)
         pca.fit(data_array)
         scores = pca.transform(data_array)
         reconstruct = pca.inverse_transform(scores)
 
         residual = numpy.sum((data_array - reconstruct) ** 2) / data_array.shape[0]
-        reconstruction_error[249 - components] = residual
+        reconstruction_error[249 - d] = residual
 
     pylab.plot(dimensions, reconstruction_error)
+    pylab.ylabel('reconstruction mse')
+    pylab.xlabel('dimensions')
     pylab.show()
-    
+
     numpy.savetxt('data.txt', data_array)
